f2406eac6a1d6fbcbd829280ef557f1b6b58787d
lantz <nathan.lantz1@etu.univ-lorraine.fr>
2024-10-24 14:10:57 +0200

    server.js mit en server.ts (typescript)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');  // Si tu as besoin de gérer les requêtes cross-origin
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'; // Si tu as besoin de gérer les requêtes cross-origin
+import dotenv from 'dotenv';
+
+// Charger les variables d'environnement
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
 
 // Middleware pour analyser les requêtes en JSON
 app.use(express.json());
 app.use(cors()); // Facultatif : si tu veux que ton backend accepte les requêtes de ton frontend Angular
 
-require('dotenv').config();
-
-// connexion à la bdd atlas
-mongoose.connect(process.env.MONGODB_URI)
+// Connexion à la base de données MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI as string)  // Assure-toi que `MONGODB_URI` est défini dans `.env`
     .then(() => {
         console.log('Connected to MongoDB Atlas');
     })
-    .catch((err) => {
+    .catch((err: Error) => {
         console.error('Error connecting to MongoDB Atlas:', err);
     });
 
-
-
 // Exemple d'une route API simple
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello from the backend!');
 });
 
