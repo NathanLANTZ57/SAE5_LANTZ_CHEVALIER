ec1ba1bc89f58860f53b3c8691b4739e45691d2b
NoemyChevalier <noemy.chevalier7@etu.univ-lorraine.fr>
2024-11-29 14:54:03 +0100

    maj formulaire

--- client/src/app/formulaire-iban/formulaire-iban.component.ts
+++ client/src/app/formulaire-iban/formulaire-iban.component.ts
@@ -16,17 +16,17 @@ export class FormulaireIbanComponent {
         '',
         [
           Validators.required,
-          Validators.pattern(/^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/), // IBAN format (34 caractères max)
-          Validators.maxLength(34) // Assure que l'IBAN ne dépasse pas 34 caractères
+          Validators.pattern(/^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/), 
+          Validators.maxLength(34) 
         ]
       ],
       bic: [
         '',
         [
           Validators.required,
-          Validators.pattern(/^[A-Z0-9]{4}[A-Z]{2}[A-Z0-9]{2}([A-Z0-9]{3})?$/), // BIC format (8 ou 11 caractères)
-          Validators.minLength(8), // Minimum : 8 caractères
-          Validators.maxLength(11) // Maximum : 11 caractères
+          Validators.pattern(/^[A-Z0-9]{4}[A-Z]{2}[A-Z0-9]{2}([A-Z0-9]{3})?$/),
+          Validators.minLength(8),
+          Validators.maxLength(11) 
         ]
       ]
     });
