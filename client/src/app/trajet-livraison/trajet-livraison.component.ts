56619abbba6c46f85e0d7286c71affa5b6bd6ca5
lantz14u <nathan.lantz1@etu.univ-lorraine.fr>
2025-01-19 15:14:22 +0100

    ajout des numéros de points et des markers sur la carte pour pouvoir voir l'ordre de l'itineraire --> problème de front et d'affichage des instruction qui apparaissent deux fois à régler

--- client/src/app/trajet-livraison/trajet-livraison.component.ts
+++ client/src/app/trajet-livraison/trajet-livraison.component.ts
@@ -12,18 +12,42 @@ export class TrajetLivraisonComponent implements OnInit {
   map!: L.Map;
   routingControl: any;
   trajets: any[] = [];
-  trajetsMardi: any[] = [];
-  trajetsMercredi: any[] = [];
-  trajetsJeudi: any[] = [];
-  trajetsVendredi: any[] = [];
-  showTuesdayOptions = false;
-  showWednesdayOptions = false;
-  showThursdayOptions = false;
-  showFridayOptions = false;
   isModalOpen = false;
-  newTrajet = { day: '', type: '', locations: [{ name: '', address: '', coords: [0, 0] }] };
+  newTrajet = { day: '', type: '', locations: [{ name: '', address: '', coords: [0, 0], pointNumber: 1 }] };
 
-  constructor(private http: HttpClient) {}
+  // Ajout des propriétés nécessaires
+  days = [
+    {
+      label: 'Mardi',
+      value: 'Mardi',
+      class: 'mardi',
+      showOptions: false,
+      trajets: []
+    },
+    {
+      label: 'Mercredi',
+      value: 'Mercredi',
+      class: 'mercredi',
+      showOptions: false,
+      trajets: []
+    },
+    {
+      label: 'Jeudi',
+      value: 'Jeudi',
+      class: 'jeudi',
+      showOptions: false,
+      trajets: []
+    },
+    {
+      label: 'Vendredi',
+      value: 'Vendredi',
+      class: 'vendredi',
+      showOptions: false,
+      trajets: []
+    }
+  ];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.initMap();
@@ -47,35 +71,51 @@ export class TrajetLivraisonComponent implements OnInit {
   }
 
   filterTrajetsByDay(): void {
-    this.trajetsMardi = this.trajets.filter((trajet) => trajet.day === 'Mardi');
-    this.trajetsMercredi = this.trajets.filter((trajet) => trajet.day === 'Mercredi');
-    this.trajetsJeudi = this.trajets.filter((trajet) => trajet.day === 'Jeudi');
-    this.trajetsVendredi = this.trajets.filter((trajet) => trajet.day === 'Vendredi');
-    console.log('Trajets par jour:', {
-      Mardi: this.trajetsMardi,
-      Mercredi: this.trajetsMercredi,
-      Jeudi: this.trajetsJeudi,
-      Vendredi: this.trajetsVendredi,
+    this.days.forEach((day) => {
+      day.trajets = this.trajets.filter((trajet) => trajet.day === day.value);
     });
+    console.log('Filtered trajets by days:', this.days);
   }
 
   showTrajet(type: string): void {
-    console.log('Clicked trajet type:', type);
     const trajet = this.trajets.find((t) => t.type === type);
-    console.log('Found trajet:', trajet);
-
     if (!trajet) {
       console.warn('Trajet not found!');
       return;
     }
 
     if (this.routingControl) {
-      console.log('Removing existing routing control...');
       this.map.removeControl(this.routingControl);
     }
 
-    console.log('Waypoints:', trajet.locations.map((location: any) => location.coords));
+    // Clear existing markers
+    this.map.eachLayer((layer: any) => {
+      if (layer.options && layer.options.icon) {
+        this.map.removeLayer(layer);
+      }
+    });
 
+    // Add numbered markers
+    trajet.locations.forEach((location: any, index: number) => {
+      const markerIcon = L.divIcon({
+        html: `<div style="position: relative;">
+                  <img src="assets/marker-icon.png" style="width: 25px; height: 41px;">
+                  <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);
+                               color: black; font-size: 12px; font-weight: bold;">
+                    ${index + 1}
+                  </span>
+               </div>`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        className: ''
+      });
+
+      L.marker(L.latLng(location.coords[0], location.coords[1]), { icon: markerIcon })
+        .addTo(this.map)
+        .bindPopup(`<b>Point ${index + 1}</b>: ${location.name}<br>${location.address}`);
+    });
+
+    // Add routing control with instructions disabled on the map
     this.routingControl = (L as any).Routing.control({
       waypoints: trajet.locations.map((location: any) =>
         L.latLng(location.coords[0], location.coords[1])
@@ -83,36 +123,56 @@ export class TrajetLivraisonComponent implements OnInit {
       routeWhileDragging: true,
       showAlternatives: true,
       lineOptions: { styles: [{ color: 'blue', weight: 4 }] },
+      createMarker: () => null, // Désactive les marqueurs de routage
+      router: new (L as any).Routing.osrmv1({
+        serviceUrl: 'https://router.project-osrm.org/route/v1',
+      }),
+      addWaypoints: false, // Empêche l'ajout de points supplémentaires
+      fitSelectedRoutes: true, // Centrer automatiquement la carte
+      show: false, // Désactive les instructions sur la carte
     }).addTo(this.map);
 
-    console.log('Routing control added.');
+    // Display instructions in the custom container
+    this.routingControl.on('routesfound', (e: any) => {
+      const instructionsContainer = document.getElementById('route-instructions');
+      if (instructionsContainer) {
+        // Videz le conteneur avant d'ajouter de nouvelles instructions
+        instructionsContainer.innerHTML = '';
+
+        // Récupérez les instructions de l'itinéraire
+        const route = e.routes[0];
+        const instructions = route.instructions.map((instr: any) =>
+          `<div>${instr.text}</div>`
+        ).join('');
+
+        // Ajoutez les nouvelles instructions dans le conteneur
+        instructionsContainer.innerHTML = `<h3>Instructions</h3>${instructions}`;
+      }
+    });
+
+
   }
 
   deleteTrajet(id: number): void {
-    console.log('Deleting trajet with id:', id);
     this.http.delete(`http://localhost:3000/api/trajets-livraison/${id}`).subscribe(() => {
       this.fetchTrajets();
     });
   }
 
-  removeLocation(index: number): void {
-    console.log('Removing location at index:', index);
-    this.newTrajet.locations.splice(index, 1);
-  }
-
   openAddModal(): void {
-    console.log('Opening modal...');
     this.isModalOpen = true;
   }
 
   closeAddModal(): void {
-    console.log('Closing modal...');
     this.isModalOpen = false;
-    this.newTrajet = { day: '', type: '', locations: [{ name: '', address: '', coords: [0, 0] }] };
+    this.newTrajet = { day: '', type: '', locations: [{ name: '', address: '', coords: [0, 0], pointNumber: 1 }] };
   }
 
   addTrajet(): void {
-    console.log('Adding new trajet:', this.newTrajet);
+    this.newTrajet.locations.forEach((location, index) => {
+      location.pointNumber = index + 1;
+    });
+
     this.http.post('http://localhost:3000/api/trajets-livraison', this.newTrajet).subscribe(() => {
       this.closeAddModal();
       this.fetchTrajets();
@@ -120,7 +180,15 @@ export class TrajetLivraisonComponent implements OnInit {
   }
 
   addLocation(): void {
-    console.log('Adding new location to trajet...');
-    this.newTrajet.locations.push({ name: '', address: '', coords: [0, 0] });
+    this.newTrajet.locations.push({
+      name: '',
+      address: '',
+      coords: [0, 0],
+      pointNumber: this.newTrajet.locations.length + 1
+    });
+  }
+
+  removeLocation(index: number): void {
+    this.newTrajet.locations.splice(index, 1);
   }
 }
